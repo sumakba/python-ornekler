4a7c25af68eacadbb71537125cbfdae473eda4ca
Batuhan <batuhansumak@gmail.com>
2017-11-09 02:41:29 +0300

    OpenCv
    resimlerle çalışmak

---
+++ opencv/opencv-resim-goruntuleme.py
@@ -0,0 +1,9 @@
+import cv2
+
+resim = cv2.imread('ataturk.jpg', cv2.IMREAD_COLOR)
+if resim is not None:
+    cv2.imshow('ilk resim', resim)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("Böyle bir resim bulunmuyor.")
